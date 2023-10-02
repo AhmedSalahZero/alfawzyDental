8ed69e80e577a7c44b7136d15ea9a7b41d86f50d
Muhammed ELsdodey <mohamedelsdodey1996@gmail.com>
2023-10-02 15:08:45 +0200

    SD7

--- resources/views/front/home/parts/slider.blade.php
+++ resources/views/front/home/parts/slider.blade.php
@@ -3,12 +3,23 @@
         <div class="min-services-container py-8 px-7 max-w-[90%] mx-auto bg-white rounded-2xl shadow-sm ">
             <div
                 class="min-services  flex flex-col lg:flex-row space-x-0  lg:space-x-5 space-y-5 lg:space-y-0  items-center justify-between    ">
-                @foreach($categoryServices as $minService)
-                    <a href="#" 
+
+                    <a target="_blank" href="{{ getWhatsappApi($settings->whatsapp) }}"
                         class="min-service grow flex items-center justify-center h-[190px] w-full lg:w-auto rounded-2xl shadow-md">
-                        <h2 class="font-semibold lg:text-xl xl:text-2xl text-black capitalize text-center">{{ $minService->title }}</h2>
+                        <h2 class="font-semibold lg:text-xl xl:text-2xl text-black capitalize text-center">Book Appointment</h2>
                     </a>
-                @endforeach
+
+                <a href="{{route('dental.tourism.index')}}"
+                   class="min-service grow flex items-center justify-center h-[190px] w-full lg:w-auto rounded-2xl shadow-md">
+                    <h2 class="font-semibold lg:text-xl xl:text-2xl text-black capitalize text-center">Dental tourism</h2>
+                </a>
+
+                <a target="_blank" href="{{ getWhatsappApi($settings->whatsapp) }}"
+                   class="min-service grow flex items-center justify-center h-[190px] w-full lg:w-auto rounded-2xl shadow-md">
+                    <h2 class="font-semibold lg:text-xl xl:text-2xl text-black capitalize text-center">Online Consultation</h2>
+                </a>
+
+
             </div>
         </div>
     </div>
