a6fd49bde04a217e551b9b30714bf2242fe35e88
Muhammed ELsdodey <mohamedelsdodey1996@gmail.com>
2023-10-02 14:57:40 +0200

    SD7

--- resources/views/front/home/parts/services.blade.php
+++ resources/views/front/home/parts/services.blade.php
@@ -1,7 +1,7 @@
 <section class="section section-bg space-between-sections-y  relative">
     <div class="res-container">
-        <h2 class="section-header text-center font-bold text-5xl text-black capitalize ">{{ __('Our Special Services') }}</h2>
-        <x-swiper.swiper :services="$special_services"></x-swiper.swiper>
+        <h2 class="section-header text-center font-bold text-5xl text-black capitalize ">Our  Services</h2>
+        <x-swiper.swiper :services="$all_services"></x-swiper.swiper>
     </div>
 </section>
 {{--
