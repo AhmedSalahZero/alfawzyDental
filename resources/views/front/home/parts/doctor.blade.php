69d3f8bfab3f5ffcb19c5b6fa11d062656b00148
AhmedSalahZero <55031551+AhmedSalahZero@users.noreply.github.com>
2023-10-09 23:12:08 +0300

    add shadow to doctors

--- resources/views/front/home/parts/doctor.blade.php
+++ resources/views/front/home/parts/doctor.blade.php
@@ -1,7 +1,7 @@
-<section class=" @if(!isset($mt)) space-between-sections-y mt-52 @else mt-0 @endif  lg:mt-0">
+<section class=" @if(!isset($mt)) space-between-sections-y mt-52 @else shadow-sm mb-24 pb-5  mt-0 @endif  lg:mt-0">
 
-    <div class="res-container @if(!isset($mt)) mt-96 md:mt-36 @endif ">
-        <div class="online-consultings relative text-left @if(!isset($mt))  mt-24 @endif mb-60  lg:my-0 flex justify-between  md:text-left items-center flex-col md:flex-row  gap-12 lg:gap-20 ">
+    <div class="res-container @if(!isset($mt)) mt-96 md:mt-36 @else  @endif ">
+        <div class="online-consultings relative text-left @if(!isset($mt))  mt-24 @else  @endif mb-60  lg:my-0 flex justify-between  md:text-left items-center flex-col md:flex-row  gap-12 lg:gap-20 ">
             <div class="online-consulting scroll-from-left-to-right">
                 <h4 class="main__title  mb-5 ">{{ __('Online Consulting') }}</h4>
                 <h2 class="sub__title  max-w-[386px] mb-5">{{ __('Make online and live Consultation easily.') }}</h2>
@@ -17,7 +17,7 @@
             <div class="online-consulting scroll-from-bottom-to-up">
                 <img src="{{asset('front/image/doctors.png')}}">
             </div>
-            <div class="online-consulting hidden md:block scroll-from-right-to-left">
+            <div class="online-consulting hidden md:block scroll-from-right-to-left @if(isset($mt))  relative top-[40px]  @endif ">
                 <img src="{{asset('front/image/doctor.png')}}">
 
             </div>
